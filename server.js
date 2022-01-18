8ad299fc34740f3f866110743d8e55ed20db4369
MikheilBregvadze <mikheil.bregvadze@geolab.edu.ge>
2022-01-18 22:19:36 +0400

    ddddd

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+app.use('/Api/Client', userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Api is running!');
+})
+
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/client/build')));
     app.get('*', (req, res) => {
@@ -32,11 +38,6 @@ if(process.env.NODE_ENV === 'production') {
     })
 }
 
-app.use('/Api/Client', userRoutes);
-
-app.get('/', (req, res) => {
-    res.send('Api is running!');
-})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));

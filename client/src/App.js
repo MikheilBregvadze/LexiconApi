68a4787c9f50184c3610994aebf06843996086e1
MikheilBregvadze <mikheil.bregvadze@geolab.edu.ge>
2022-02-11 18:24:55 +0400

    add landing animation

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./pages/layout/header/Header";
 import { Auth, useAuthentication } from "./services/context/useAuthentication";
 import MainPage from "./pages/mainPage/MainPage";
+import Landing from "./pages/landing/Landing";
 
 import './App.css';
 
@@ -8,10 +9,11 @@ function App() {
     const auth = useAuthentication();
     return (
         <Auth.Provider value={auth}>
+            <Landing />
             <header className="header">
                 <Header />
             </header>
-            <section>
+            <section className="section">
                 <MainPage />
             </section>
         </Auth.Provider>

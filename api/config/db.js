3a7bfc0c017ecc7aa5726412fe0b921f3ce4b4c8
SMARTSOFT\m.bregvadze <mbregvadze@smartsoft.ge>
2023-11-24 18:19:45 +0400

    ...??

--- api/config/db.js
+++ api/config/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        console.log(process.env.MONGO_URI);
-        mongoose.set("strictQuery", false);
+        console.log('.');
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true
